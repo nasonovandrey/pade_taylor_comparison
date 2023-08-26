92bf182f6fefb9c78546aedc2b21be2b203e6a57
nasonovandrey <andrey.m.nasonov@gmail.com>
2023-08-27 00:56:42 +0600

    Add suggestions on horizontal shifts and error handling

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import sympy as sp
 from scipy.interpolate import pade
+from sympy.core.function import PoleError
 
 
 def refine_x_values_around_singularities(x_values, threshold=1e-3):
@@ -151,73 +152,104 @@ if func_str:
     original_x_values = np.linspace(-x_scale, x_scale, 400)
     x_values = refine_x_values_around_singularities(original_x_values)
     y_values = [evaluate_expression(func_str, val) for val in x_values]
+    if any(isinstance(val, complex) for val in y_values):
+        st.warning("The function produces complex values and cannot be plotted.")
+    else:
+        if isinstance(y_values[0], (int, float, np.number)):
+            plt.figure(figsize=(8, 6))
+            plt.plot(x_values, y_values, label=func_str)
 
-    if isinstance(y_values[0], (int, float, np.number)):
-        plt.figure(figsize=(8, 6))
-        plt.plot(x_values, y_values, label=func_str)
-
-        # If user selects Taylor's approximation
-        if show_taylor and taylor_order is not None:
             x_symbol = sp.symbols("x")
             func_sympy = sp.sympify(func_str)
-            taylor_expansion = func_sympy.series(
-                x_symbol, 0, taylor_order + 1
-            ).removeO()
+            taylor_expansion = None
+            try:
+                taylor_expansion = func_sympy.series(
+                    x_symbol, 0, taylor_order + 1
+                ).removeO()
+            except PoleError:
+                st.error(
+                    f"Cannot expand {func_str} around x=0. Consider a horizontal shift (e.g., use `x-a` instead of `x`)"
+                )
+            # If the Taylor expansion is a constant function around zero
+            if taylor_expansion is not None and taylor_expansion.is_number:
+                st.warning(
+                    "The Taylor expansion for this function around x=0 is a constant. "
+                    "Consider a horizontal shift (e.g., use `x-a` instead of `x`) "
+                    "for a more meaningful Taylor approximation."
+                )
+            # If user selects Taylor's approximation
+            if (
+                show_taylor
+                and taylor_order is not None
+                and taylor_expansion is not None
+                and not taylor_expansion.is_number
+            ):
+                # Display the Taylor's approximation expression on the sidebar
+                st.sidebar.write(f"Taylor's Approximation (Order {taylor_order}):")
+                st.sidebar.latex(sp.latex(taylor_expansion))
 
-            # Display the Taylor's approximation expression on the sidebar
-            st.sidebar.write(f"Taylor's Approximation (Order {taylor_order}):")
-            st.sidebar.latex(sp.latex(taylor_expansion))
+                taylor_func = sp.lambdify(x_symbol, taylor_expansion, "numpy")
+                y_taylor_result = taylor_func(x_values)
 
-            taylor_func = sp.lambdify(x_symbol, taylor_expansion, "numpy")
-            y_taylor = taylor_func(x_values)
-
-            plt.plot(
-                x_values, y_taylor, label=f"Taylor Approximation (Order {taylor_order})"
-            )
-
-        # If user selects Pade approximation
-        if show_pade and taylor_order is not None:
-            x_symbol = sp.symbols("x")
-            func_sympy = sp.sympify(func_str)
-            taylor_expansion = func_sympy.series(
-                x_symbol, 0, taylor_order + 1
-            ).removeO()
-            taylor_coeffs = [
-                taylor_expansion.coeff(x_symbol, i) for i in range(taylor_order + 1)
-            ]
-
-            n = taylor_order // 2
-            m = taylor_order - taylor_order // 2
-            p, q = compute_pade(taylor_coeffs, n, m)
-            if p is not None and q is not None:
-                # Create callable polynomial functions for p and q
-                p_func = np.poly1d(p.coeffs)
-                q_func = np.poly1d(q.coeffs)
-
-                # Evaluate the Pade approximation
-                y_pade = [p_func(val) / q_func(val) for val in x_values]
+                # Check if y_taylor_result is scalar and, if so, convert it to an array
+                if np.isscalar(y_taylor_result):
+                    y_taylor = np.full(x_values.shape, y_taylor_result)
+                else:
+                    y_taylor = y_taylor_result
 
                 plt.plot(
                     x_values,
-                    y_pade,
-                    label=f"Pade Approximation (Numerator Order {n}, Denominator Order {m})",
+                    y_taylor,
+                    label=f"Taylor Approximation (Order {taylor_order})",
                 )
-                pade_expression = generate_pade_expression(p, q)
-                st.sidebar.write(
-                    f"Pade Approximation (Numerator Order {n}, Denominator Order {m}):"
-                )
-                st.sidebar.latex(pade_expression)
 
-        # Center the coordinate plane around (0, 0) and set the scale
-        plt.xlim([-x_scale, x_scale])
-        plt.ylim([-y_scale, y_scale])
-        plt.axhline(0, color="black", linewidth=0.5)
-        plt.axvline(0, color="black", linewidth=0.5)
-        plt.title("y = f(x)")
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.legend()
-        plt.grid(True)
-        st.pyplot(plt)
-    else:
-        st.error(y_values[0])
+            # If user selects Pade approximation
+            if (
+                show_pade
+                and taylor_order is not None
+                and taylor_expansion is not None
+                and not taylor_expansion.is_number
+            ):
+                taylor_coeffs = [
+                    taylor_expansion.coeff(x_symbol, i) for i in range(taylor_order + 1)
+                ]
+
+                n = taylor_order // 2
+                m = taylor_order - taylor_order // 2
+                try:
+                    p, q = compute_pade(taylor_coeffs, n, m)
+                    if p is not None and q is not None:
+                        # Create callable polynomial functions for p and q
+                        p_func = np.poly1d(p.coeffs)
+                        q_func = np.poly1d(q.coeffs)
+
+                        # Evaluate the Pade approximation
+                        y_pade = [p_func(val) / q_func(val) for val in x_values]
+                        plt.plot(
+                            x_values,
+                            y_pade,
+                            label=f"Pade Approximation (Numerator Order {n}, Denominator Order {m})",
+                        )
+                        pade_expression = generate_pade_expression(p, q)
+                        st.sidebar.write(
+                            f"Pade Approximation (Numerator Order {n}, Denominator Order {m}):"
+                        )
+                        st.sidebar.latex(pade_expression)
+                except TypeError:
+                    st.warning(
+                        "Pade approximation resulted in complex coefficients and cannot be plotted."
+                    )
+
+            # Center the coordinate plane around (0, 0) and set the scale
+            plt.xlim([-x_scale, x_scale])
+            plt.ylim([-y_scale, y_scale])
+            plt.axhline(0, color="black", linewidth=0.5)
+            plt.axvline(0, color="black", linewidth=0.5)
+            plt.title("y = f(x)")
+            plt.xlabel("x")
+            plt.ylabel("y")
+            plt.legend()
+            plt.grid(True)
+            st.pyplot(plt)
+        else:
+            st.error(y_values[0])
